792a1b610c91a107c47b7bc5f70134fda9de2a59
Thallyta Castro <thallyta180136319@gmail.com>
2021-10-17 22:49:20 -0300

    Primeiro commit repositorio

---
+++ js/owl/index.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+
+
